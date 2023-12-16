a1a8af5c1478db444efba813a89a15bf70ff8c8d
alizeinodin <alizeinodin79@gmail.com>
2023-12-16 23:45:18 +0330

    Use Heading in Brands

--- src/components/Brands.vue
+++ src/components/Brands.vue
@@ -1,59 +1,66 @@
 <template>
+  <div class="w-ful bg-slate-200 block">
+    <div class="container mx-auto h-40 pt-10 text-center">
+      <Heading
+          subtitle="همکاران ما" ,
+          title="ما با چه شرکت هایی همکاری کرده ایم؟"
+      ></Heading>
+    </div>
 
-  <div class="xl:h-28 lg:h-14 md:h-10 bg-gray-50 hidden md:block  px-[15%]">
-  
-    <swiper
-        :slides-per-view="6"
-        :space-between="10"
-        allow-touch-move
-        autoplay
-        navigation
-        :pagination="{ clickable: true }"
-        :scrollbar="{ draggable: true }"
-        grid
-        grab-cursor
-        modules="modules"
-        rewind
+    <div class="xl:h-28 lg:h-14 md:h-10 hidden md:block  px-[15%]">
+      <swiper
+          :slides-per-view="6"
+          :space-between="10"
+          allow-touch-move
+          autoplay
+          navigation
+          :pagination="{ clickable: true }"
+          :scrollbar="{ draggable: true }"
+          grid
+          grab-cursor
+          modules="modules"
+          rewind
 
-        @swiper="onSwiper"
-        @slideChange="onSlideChange"
-    >
-      <swiper-slide v-for="img in images" :key="img"
-                    class="xl:min-h-[100px] lg:min-h-[70px] md:min-h-[50px] mx-auto flex ">
-        <div class="xl:w-[70%] lg:w-[80%] md:w-[70%]">
-          <a :href="img.link">
-            <img class="grayscale hover:grayscale-0 transition-all duration-1000 xl:mt-10 lg:mt-4 md:mt-3"
-                 :src="img.src" alt="">
-          </a>
-        </div>
-      </swiper-slide>
-    </swiper>
-  </div>
-  <div class="bg-gray-50 h-8 md:hidden px-[10%]">
-    <swiper
-        :slides-per-view="3"
-        :space-between="10"
-        allow-touch-move
-        autoplay
-        navigation
-        :pagination="{ clickable: true }"
-        :scrollbar="{ draggable: true }"
-        grid
-        grab-cursor
-        modules="modules"
-        rewind
+          @swiper="onSwiper"
+          @slideChange="onSlideChange"
+      >
+        <swiper-slide v-for="img in images" :key="img"
+                      class="xl:min-h-[100px] lg:min-h-[70px] md:min-h-[50px] mx-auto flex ">
+          <div class="xl:w-[70%] lg:w-[80%] md:w-[70%]">
+            <a :href="img.link">
+              <img class="grayscale hover:grayscale-0 transition-all duration-1000 xl:mt-10 lg:mt-4 md:mt-3"
+                   :src="img.src" alt="">
+            </a>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+    <div class="bg-gray-50 h-8 md:hidden px-[10%]">
+      <swiper
+          :slides-per-view="3"
+          :space-between="10"
+          allow-touch-move
+          autoplay
+          navigation
+          :pagination="{ clickable: true }"
+          :scrollbar="{ draggable: true }"
+          grid
+          grab-cursor
+          modules="modules"
+          rewind
 
-        @swiper="onSwiper"
-        @slideChange="onSlideChange"
-    >
-      <swiper-slide v-for="img in images" :key="img" class="min-h-[40px] mx-auto flex ">
-        <div class="w-[70%]">
-          <a :href="img.link">
-            <img class="grayscale hover:grayscale-0 transition-all duration-1000 mt-1.5" :src="img.src" alt="">
-          </a>
-        </div>
-      </swiper-slide>
-    </swiper>
+          @swiper="onSwiper"
+          @slideChange="onSlideChange"
+      >
+        <swiper-slide v-for="img in images" :key="img" class="min-h-[40px] mx-auto flex ">
+          <div class="w-[70%]">
+            <a :href="img.link">
+              <img class="grayscale hover:grayscale-0 transition-all duration-1000 mt-1.5" :src="img.src" alt="">
+            </a>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 
 </template>
@@ -63,6 +70,7 @@ import {Swiper, SwiperSlide} from 'swiper/vue';
 
 // Import Swiper styles
 import 'swiper/css';
+import Heading from "@/components/Heading.vue";
 
 export default {
   data() {
@@ -100,6 +108,7 @@ export default {
     }
   },
   components: {
+    Heading,
     Swiper,
     SwiperSlide,
   },
