a97c2d13ce245e4478c3d7db70177ac6989125ff
alizeinodin <alizeinodin79@gmail.com>
2023-12-16 23:13:28 +0330

    Add icons

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import {RouterView} from 'vue-router'
 import Header from "@/components/Header.vue";
 import Slider from "@/components/Slider.vue";
 import Brands from "@/components/Brands.vue";
+import Products from "@/components/Products.vue"
 </script>
 
 <template>
   <div dir="rtl">
     <Header></Header>
     <Slider></Slider>
+    <Products></Products>
     <Brands></Brands>
 
     <RouterView/>
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,36 @@
-
 import './assets/index.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+import {library} from '@fortawesome/fontawesome-svg-core'
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-import { faArrowRightLong, faArrowLeftLong, faHouseChimney, faLaptop, faBuilding, faPhone } from '@fortawesome/free-solid-svg-icons'
+import {
+    faArrowLeftLong,
+    faArrowRightLong,
+    faBuilding,
+    faCode,
+    faHouseChimney,
+    faLaptop,
+    faPhone
+} from '@fortawesome/free-solid-svg-icons'
 
 
 /* add icons to the library */
-library.add(faArrowRightLong, faArrowLeftLong, faHouseChimney, faLaptop, faBuilding, faPhone)
+library.add(
+    faArrowRightLong,
+    faArrowLeftLong,
+    faHouseChimney,
+    faLaptop,
+    faBuilding,
+    faPhone,
+    faCode
+)
 
 
 const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
