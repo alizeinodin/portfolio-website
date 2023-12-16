9629e8b96b2949294c676d1cefcbc3b435d18131
alizeinodin <alizeinodin79@gmail.com>
2023-12-16 23:20:47 +0330

    Add Heading component

---
+++ src/components/Heading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="text-sm font-thin text-zinc-400">
+      { {{ this.subtitle }} }
+    </p>
+  </div>
+  <div class="mt-2">
+    <h2 class="text-2xl font-bold text-gray-600">
+      {{ this.title }}
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['title', 'subtitle']
+}
+</script>
+
+<style scoped>
+
+</style>
