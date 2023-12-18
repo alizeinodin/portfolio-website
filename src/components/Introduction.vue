0a578291bb5c38d96824edf36e19b8a6400a9311
alizeinodin <alizeinodin79@gmail.com>
2023-12-19 00:40:29 +0330

    Add introduction component

---
+++ src/components/Introduction.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="py-48 bg-gray-50">
+    <div class="container mx-auto flex justify-between content-center">
+    <div class="flex-1 px-4">
+      <div class="flex flex-col h-72 justify-between">
+        <div>
+          <h3 class="text-sm font-thin text-zinc-400">  در مورد ما</h3>
+        </div>
+        <div>
+          <h1 class="text-4xl font-medium text-zinc-800 text-justify"> ما بهترین شریک تجاری شما
+برای نوآوری کسب و کار هستیم </h1>
+        </div>
+        <div>
+          <p class="text-justify text-zinc-500">
+            لورم ایپسوم یک متن ساختگی و نامفهوم برای طراحان وب است تا محتوای پیشفرض را برای پر کردن صفحات وب وارد کنند. در حقیقت این متن هیچ معنی و مفهوم خاصی ندارد و فقط برای پر کردن صفحه از آن استفاده میشود تا کاربران سایت را به همراه محتوا ببینند.
+          </p>
+        </div>
+      </div>
+
+      <div class="flex justify-between mt-20 ">
+        <div class="flex-1 flex-col h-24 justify-between pl-4">
+          <div>
+            <h2 class="text-xl text-indigo-900">
+                تجربه
+            </h2>
+          <div class="border mt-2"></div>
+
+          </div>
+          <div class="mt-4 text-zinc-500">
+            <p>
+              تیم بزرگ ما از بیش از 1400 متخصص نرم افزار.
+            </p>
+          </div>
+        </div>
+        <div class="flex-1 flex-col justify-between pr-4">
+          <div>
+            <h2 class="text-xl text-indigo-900">
+              پشتیبانی سریع
+            </h2>
+          <div class="border mt-2"></div>
+
+          </div>
+          <div class="mt-4 text-zinc-500">
+            <p>
+              ما به شما کمک خواهیم کرد تا ضمن به اشتراک گذاشتن ، ایده های جدید و جسورانه را آزمایش کنید.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="flex-2">
+      <img src="https://frontdesign.ir/template/engitech/wp-content/uploads/2020/04/image1-home1.png">
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
