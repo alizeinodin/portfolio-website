0135528ed928749a4afd11e4211f4163fc91dfcb
alizeinodin <alizeinodin79@gmail.com>
2023-12-21 18:06:09 +0330

    Add flexibility of Heading component

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -5,7 +5,7 @@
     </p>
   </div>
   <div class="mt-2">
-    <h2 class="text-2xl font-bold text-gray-600">
+    <h2 class="text-2xl font-bold" :class="this.color">
       {{ this.title }}
     </h2>
   </div>
@@ -13,7 +13,18 @@
 
 <script lang="ts">
 export default {
-  props: ['title', 'subtitle']
+  props: {
+    title: {
+      type: String,
+    },
+    subtitle: {
+      type: String,
+    },
+    color: {
+      type: String,
+      default: "text-gray-500"
+    }
+  }
 }
 </script>
 
