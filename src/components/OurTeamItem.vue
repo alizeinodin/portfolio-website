d6e9ecef9d2638b3bef81030d1e620c30bdf91ba
alizeinodin <alizeinodin79@gmail.com>
2023-12-21 18:41:32 +0330

    Add justify class to OurTeamItem.vue component

--- src/components/OurTeamItem.vue
+++ src/components/OurTeamItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-col transition-all duration-700 bg-gray-50 cursor-pointer pb-4 justify-between items-center text-center hover:bg-white hover:shadow-2xl">
+  <div class="flex flex-col transition-all duration-700 bg-gray-50 cursor-pointer justify-between items-center text-center hover:bg-white hover:shadow-2xl">
     <div class="">
       <img :src=this.img alt="{{ this.name }}">
     </div>
-    <div class="mt-5">
+    <div class="flex flex-col justify-center py-5">
       <div>
         <h2 class="text-2xl text-gray-950">{{ this.name }}</h2>
       </div>
