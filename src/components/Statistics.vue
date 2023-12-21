0410e12dba0f34e0979c5d4d6574afdfed4fd316
alizeinodin <alizeinodin79@gmail.com>
2023-12-21 18:03:56 +0330

    Add statistics

---
+++ src/components/Statistics.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="pb-20 pt-10 bg-indigo-700">
+    <div class="container h-32 mx-auto text-center">
+      <Heading
+          subtitle="آمار ما"
+          title="موفقیت ما بی دلیل نیست!"
+          color="text-gray-100"
+      >
+      </Heading>
+    </div>
+    <div class="container flex justify-between mx-auto">
+      <div>
+        <StatisticsItem
+            number="30"
+            operator="+"
+            description="سال تجربه کاری موفق"
+        >
+        </StatisticsItem>
+      </div>
+      <div>
+        <StatisticsItem
+            number="97"
+            operator="%"
+            description="درصد رضایت کامل مشتریان"
+        >
+        </StatisticsItem>
+      </div>
+      <div>
+        <StatisticsItem
+            number="746"
+            operator="+"
+            description="پروژه موفق"
+        >
+        </StatisticsItem>
+      </div>
+      <div>
+        <StatisticsItem
+            number="100"
+            operator="+"
+            description="عدد قهوه در ساعت"
+        >
+        </StatisticsItem>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import StatisticsItem from "@/components/StatisticsItem.vue"
+import Heading from "@/components/Heading.vue";
+
+export default {
+  components: {Heading, StatisticsItem}
+
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/StatisticsItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-between items-center space-x-2">
+    <div>
+      <p class="text-white text-5xl font-bold">
+        <vue3-autocounter ref='counter' :startAmount='0' :endAmount=this.number :duration='3'
+                          separator=','/>
+      </p>
+    </div>
+    <div class="px-3">
+      <p class="text-amber-500 font-semibold text-3xl"> {{ this.operator }} </p>
+    </div>
+    <div class="w-1/2">
+      <p class="text-lg font-thin text-slate-200"> {{ this.description }}</p>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts">
+export default {
+  props: ['number', 'operator', 'description'],
+}
+</script>
+
+<style scoped>
+
+</style>
