686ae64cbaf52a92fd3099a3aa16ba01f2af726f
alizeinodin <alizeinodin79@gmail.com>
2023-12-22 22:15:45 +0330

    Add HeaderPage component

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="h-96 w-full bg-[url('/img/bg-pheader.jpg')] flex items-center">
+    <div class="container mr-60 flex">
+      <div>
+        <h1 class="text-gray-50 text-5xl">درباره ما</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
