3e74825ea78cb0b9e62191991fcb803f0d8530f5
alizeinodin <alizeinodin79@gmail.com>
2023-12-22 21:49:25 +0330

    Add responsive styles to commentItem

--- src/components/CommentItem.vue
+++ src/components/CommentItem.vue
@@ -2,11 +2,11 @@
   <div class="flex flex-col p-10 bg-white mx-5 mb-2.5 shadow-shadow-1">
     <div class="flex justify-start items-center">
       <div class="flex">
-        <img :src=this.avatar :alt=this.name>
+        <img class="lg:w-full w-[70%]" :src=this.avatar :alt=this.name>
       </div>
       <div class="mr-4">
         <div class="flex flex-col">
-          <div class="text-gray-950 lg:text-xl text-md">
+          <div class="text-gray-950 lg:text-xl text-base">
             <h2>{{ this.name }}</h2>
           </div>
           <div class="lg:text-sm text-xs text-gray-400 mt-2">
@@ -15,7 +15,7 @@
         </div>
       </div>
     </div>
-    <div class="text-justify text-sm lg:text-md text-gray-500 mt-8">
+    <div class="text-justify text-xs lg:text-base text-gray-500 mt-8">
         <p>
           {{ this.text }}
         </p>
