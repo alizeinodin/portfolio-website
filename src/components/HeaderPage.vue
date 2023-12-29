c6f91ff2715b017d696eb95b26abb51493c38fbd
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 17:58:28 +0330

    Add flexibility to HeaderPage component

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,12 +1,19 @@
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "درباره ما"
+    }
+  }
+}
 </script>
 
 <template>
   <div class="h-96 w-full bg-[url('/img/bg-pheader.jpg')] flex items-center">
-    <div class="container mr-60 flex">
+    <div class="container lg:mr-60 mr-10 flex">
       <div>
-        <h1 class="text-gray-50 text-5xl">درباره ما</h1>
+        <h1 class="text-gray-50 text-5xl">{{ title }}</h1>
       </div>
     </div>
   </div>
