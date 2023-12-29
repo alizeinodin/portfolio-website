5b0313e1b1d11aead97889dfb5a3767b323777e0
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 17:57:39 +0330

    Delete additional this

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <p class="lg:text-sm text-xs font-thin text-zinc-400">
-      { {{ this.subtitle }} }
+      { {{ subtitle }} }
     </p>
   </div>
   <div class="mt-2">
-    <h2 class="lg:text-2xl text-sm font-bold" :class="this.color">
-      {{ this.title }}
+    <h2 class="lg:text-2xl text-sm font-bold" :class="color">
+      {{ title }}
     </h2>
   </div>
 </template>
