456f4b5eadc076b5aadb88fe492a1061c812c3e9
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 17:59:36 +0330

    Delete additional 'this'

--- src/components/CommentItem.vue
+++ src/components/CommentItem.vue
@@ -2,22 +2,22 @@
   <div class="flex flex-col p-10 bg-white mx-5 mb-2.5 shadow-shadow-1">
     <div class="flex justify-start items-center">
       <div class="flex">
-        <img class="lg:w-full w-[70%]" :src=this.avatar :alt=this.name>
+        <img class="lg:w-full w-[70%]" :src=avatar :alt=name>
       </div>
       <div class="mr-4">
         <div class="flex flex-col">
           <div class="text-gray-950 lg:text-xl text-base">
-            <h2>{{ this.name }}</h2>
+            <h2>{{ name }}</h2>
           </div>
           <div class="lg:text-sm text-xs text-gray-400 mt-2">
-            <p> {{ this.position }} </p>
+            <p> {{ position }} </p>
           </div>
         </div>
       </div>
     </div>
     <div class="text-justify text-xs lg:text-base text-gray-500 mt-8">
         <p>
-          {{ this.text }}
+          {{ text }}
         </p>
     </div>
   </div>
