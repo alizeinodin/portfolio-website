c1ecb7acf7d7af389a98a5ba0d1c40566d88929c
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 17:59:05 +0330

    Add ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import HeaderPage from "@/components/HeaderPage.vue";
+import ContactUs from '@/components/ContactUs.vue'
 </script>
 
 <template>
-  
+    <Header></Header>
+    <HeaderPage
+        title="ارتباط با ما"
+    ></HeaderPage>
+    
+    <ContactUs></ContactUs>
+
+    <Footer></Footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
