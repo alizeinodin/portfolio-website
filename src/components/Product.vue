bdcfcb2849be38310a00f4b98eac82f8dcecf335
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 18:06:28 +0330

    Delete additional 'this'

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,18 +3,18 @@
     <div class="flex justify-between">
       <div class="flex-1">
         <font-awesome-icon class="lg:text-4xl text-2xl text-indigo-950 hover:text-yellow-600 transition-all duration-1000"
-                           :icon="this.logo"/>
+                           :icon="logo"/>
       </div>
 
       <div class="flex-2 flex-col justify-start w-[85%] h-20">
         <div class="mr-2 text-right">
           <h2 class="lg:text-xl text-md font-bold text-slate-800">
-            {{ this.title }}
+            {{ title }}
           </h2>
         </div>
         <div class="w-[90%] lg:w-full">
           <p class="flex flex-wrap text-justify text-gray-600 lg:px-0 mt-5 text-sm">
-            {{ this.description }}
+            {{ description }}
           </p>
         </div>
       </div>
