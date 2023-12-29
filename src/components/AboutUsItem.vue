76439b8c01ea3c7733338d62d98997a0ff32cf99
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 18:11:14 +0330

    Delete additional 'this'

--- src/components/AboutUsItem.vue
+++ src/components/AboutUsItem.vue
@@ -17,12 +17,12 @@ export default {
 <template>
   <div class="relative">
     <div class="absolute top-1/2 right-1/2 text-2xl text-white z-10">
-      <h2>{{ this.title }}</h2>
+      <h2>{{ title }}</h2>
     </div>
     
     <div>
-      <a :href="this.link">
-      <img class="grayscale hover:grayscale-0 duration-700 rounded-lg" :src="this.img" :alt="this.title">
+      <a :href="link">
+      <img class="grayscale hover:grayscale-0 duration-700 rounded-lg" :src="img" :alt="title">
     </a>
     </div>
   </div>
