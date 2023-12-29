25a1d4e1f1788fc96ca7740717cea157d9aa2a58
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 18:08:58 +0330

    Delete additional 'this'

--- src/components/StatisticsItem.vue
+++ src/components/StatisticsItem.vue
@@ -2,15 +2,14 @@
   <div class="flex justify-between items-center space-x-2">
     <div>
       <p class="text-white lg:text-5xl text-2xl font-bold">
-        <vue3-autocounter ref='counter' :startAmount='0' :endAmount=this.number :duration='3'
-                          separator=','/>
+        {{ number }}
       </p>
     </div>
     <div class="px-3">
-      <p class="text-amber-500 lg:font-semibold font-bold lg:text-3xl text-2xl"> {{ this.operator }} </p>
+      <p class="text-amber-500 lg:font-semibold font-bold lg:text-3xl text-2xl"> {{ operator }} </p>
     </div>
     <div class="w-1/2">
-      <p class="lg:text-lg text-xs font-thin text-slate-200"> {{ this.description }}</p>
+      <p class="lg:text-lg text-xs font-thin text-slate-200"> {{ description }}</p>
     </div>
   </div>
 
