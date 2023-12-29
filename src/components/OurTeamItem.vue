b7e25a01738a96035535f1a8ea7ab669ca7ff888
alizeinodin <alizeinodin79@gmail.com>
2023-12-29 18:06:20 +0330

    Delete additional 'this'

--- src/components/OurTeamItem.vue
+++ src/components/OurTeamItem.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex flex-col transition-all duration-700 bg-gray-50 cursor-pointer justify-between items-center text-center hover:bg-white hover:shadow-2xl">
     <div class="">
-      <img :src=this.img alt="{{ this.name }}">
+      <img :src=img alt="{{ this.name }}">
     </div>
     <div class="flex flex-col justify-center py-5">
       <div>
-        <h2 class="lg:text-2xl text-md text-gray-950">{{ this.name }}</h2>
+        <h2 class="lg:text-2xl text-md text-gray-950">{{ name }}</h2>
       </div>
       <div>
-        <p class="lg:text-sm text-xs text-gray-600 mt-2">{{ this.position }}</p>
+        <p class="lg:text-sm text-xs text-gray-600 mt-2">{{ position }}</p>
       </div>
     </div>
   </div>
