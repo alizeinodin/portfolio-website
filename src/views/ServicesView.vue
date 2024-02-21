87d2133cc21ae1d94510c1b17724d43fb97cd31e
alizeinodin <alizeinodin79@gmail.com>
2024-02-21 20:32:21 +0330

    Add ServicesView

---
+++ src/views/ServicesView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+import Header from "@/components/Header.vue";
+import HeaderPage from "@/components/HeaderPage.vue";
+import Footer from "@/components/Footer.vue";
+import Services from "@/components/Services.vue";
+</script>
+
+<template>
+  <main>
+    <Header></Header>
+    <HeaderPage
+        title="خدمات ما"
+    ></HeaderPage>
+    <Services></Services>
+    <Footer></Footer>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
